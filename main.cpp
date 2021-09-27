0ed9adb7ba5d5f4b734dd9ba14d7bdc03fd8be7f
Vitor Felipe Ramos Mello <vitorfeliperamosmello@edu.univali.br>
2021-09-27 15:47:03 -0300

    commit dev

--- main.cpp
+++ main.cpp
@@ -1,12 +1,46 @@
-# include "Canvas.h"
+#include "Canvas.h"
 #include <iostream>
+#include "SDL.h"
 
-void main()
-{
 
-}
+int main(int argc, char* argv[]) {
 
-void NA()
-{
+    SDL_Window* window;                    // Declare a pointer
 
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "An SDL2 window",                  // window title
+        SDL_WINDOWPOS_CENTERED,           // initial x position
+        SDL_WINDOWPOS_CENTERED,           // initial y position
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+
+    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean up
+    SDL_Quit();
+    return 0;
 }
