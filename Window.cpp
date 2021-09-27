6015f3a1e65ca08dbb09c2b81556bf924063fe0c
Gabriel Prates <prates@edu.univali.br>
2021-09-27 16:04:44 -0300

    Commit

--- Window.cpp
+++ Window.cpp
@@ -1,48 +1,127 @@
-# include "Canvas.h"
-#include <iostream>
-#include "SDL.h"
+#include "Window.h"
 
+Window::Window()
+{
 
+    //Initialize non-existant window
+    mWindow = NULL;
+    mMouseFocus = false;
+    mKeyboardFocus = false;
+    mFullScreen = false;
+    mMinimized = false;
+    mWidth = 0;
+    mHeight = 0;
 
-int main(int argc, char* argv[]) {
+}
 
-    SDL_Window* window;                    // Declare a pointer
-
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
-
-    // Create an application window with the following settings:
-    window = SDL_CreateWindow(
-        "An SDL2 window",                  // window title
-        SDL_WINDOWPOS_UNDEFINED,           // initial x position
-        SDL_WINDOWPOS_UNDEFINED,           // initial y position
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_OPENGL                  // flags - see below
-    );
-
-    // Check that the window was successfully created
-    if (window == NULL) {
-        // In the case that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
+bool Window::init()
+{
+    //Create window
+    mWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 5, 5, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if (mWindow != NULL)
+    {
+        mMouseFocus = true;
+        mKeyboardFocus = true;
+        mWidth = 5;
+        mHeight = 5;
     }
 
-    SDL_Event event;
-    bool running = true;
 
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = false;
-            }
+    return mWindow != NULL;
+}
+
+SDL_Renderer* Window::createRenderer()
+{
+    return SDL_CreateRenderer(mWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+}
+
+void Window::handleEvent(SDL_Event& e)
+{
+    //Window event occured
+    if (e.type == SDL_WINDOWEVENT)
+    {
+        //Caption update flag
+        bool updateCaption = false;
+        switch (e.window.event)
+        {
+            //Get new dimensions and repaint on window size change
+        case SDL_WINDOWEVENT_SIZE_CHANGED:
+            mWidth = e.window.data1;
+            mHeight = e.window.data2;
+            SDL_RenderPresent(gRenderer);
+            break;
+
+            //Repaint on exposure
+        case SDL_WINDOWEVENT_EXPOSED:
+            SDL_RenderPresent(gRenderer);
+            break;
+            //Mouse entered window
+        case SDL_WINDOWEVENT_ENTER:
+            mMouseFocus = true;
+            updateCaption = true;
+            break;
+
+            //Mouse left window
+        case SDL_WINDOWEVENT_LEAVE:
+            mMouseFocus = false;
+            updateCaption = true;
+            break;
+
+            //Window has keyboard focus
+        case SDL_WINDOWEVENT_FOCUS_GAINED:
+            mKeyboardFocus = true;
+            updateCaption = true;
+            break;
+
+            //Window lost keyboard focus
+        case SDL_WINDOWEVENT_FOCUS_LOST:
+            mKeyboardFocus = false;
+            updateCaption = true;
+            break;
+            //Window minimized
+        case SDL_WINDOWEVENT_MINIMIZED:
+            mMinimized = true;
+            break;
+
+            //Window maxized
+        case SDL_WINDOWEVENT_MAXIMIZED:
+            mMinimized = false;
+            break;
+
+            //Window restored
+        case SDL_WINDOWEVENT_RESTORED:
+            mMinimized = false;
+            break;
         }
     }
+}
 
-    // The window is open: could enter program loop here (see SDL_PollEvent()
-    // Close and destroy the window
-    SDL_DestroyWindow(window);
-    // Clean up
-    SDL_Quit();
-    return 0;
+void Window::free()
+{
+}
+
+int Window::getWidth()
+{
+    return mWidth;
+}
+
+int Window::getHeight()
+{
+    return mHeight;
+}
+
+bool Window::hasMouseFocus()
+{
+    return mMouseFocus;
+}
+
+bool Window::hasKeyboardFocus()
+{
+    return mKeyboardFocus;
+}
+
+bool Window::isMinimized()
+{
+    return mMinimized;
 }
 
